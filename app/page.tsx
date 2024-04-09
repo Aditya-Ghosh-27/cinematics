17a93df0fd2491852859a569f0b694103396659b
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-04-10 00:32:20 +0530

    initial commit and routes initialised

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen text-purple-500 text-3xl">
+      <h1>Hello from Next 13🔥</h1>
+    </main>
+  );
+}
